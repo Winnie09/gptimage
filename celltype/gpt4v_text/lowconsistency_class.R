ac5210c1103bd8e7e758b11e43979fddb05bbfa0
Wenpin Hou <whou10@jhu.edu>
2023-12-31 13:08:54 -0500

    celltype/

---
+++ celltype/gpt4v_text/lowconsistency_class.R
@@ -0,0 +1,10 @@
+library(rjson)
+
+af <- list.files(paste0('/Users/zj67/Dropbox/research/gptimage/livecell/test/'))
+for (fid in 1:5) {
+  d <- fromJSON(file=paste0('/Users/zj67/Dropbox/research/gptimage/livecell/extrun/gpt4v_verbal/feature/',fid,'.json'))
+  for (f in af) {
+    system(paste0('python3 /Users/zj67/Dropbox/research/gptimage/software/gpt4v_verbalclass.py /Users/zj67/Dropbox/research/gptimage/livecell/test/',f,' "Is the given image more consistent with row 1 or row 2 based on the descriptions below ?\n',d$choices[[1]]$message$content,'" /Users/zj67/Dropbox/research/gptimage/livecell/extrun/gpt4v_verbal/class/',fid,'_',f,'.json'))
+  }
+}
+
